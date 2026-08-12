d670eb12080377223157b4c8ec7d228642122d59
João Guilherme Oliveira Pereira <oliveiralaive15@gmail.com>
2026-08-12 16:55:20 -0300

    Criação de SecurityConfigurations

---
+++ src/main/java/Projeto/Gerenciador_Eventos/security/SecurityConfigurations.java
@@ -0,0 +1,46 @@
+package Projeto.Gerenciador_Eventos.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfigurations {
+
+	@Autowired
+	private SecurityFilter securityFilter;
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(csrf -> csrf.disable());
+		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		http.authorizeHttpRequests(requisicoes -> {
+			requisicoes.requestMatchers(HttpMethod.POST, "/login").permitAll();
+			requisicoes.requestMatchers(HttpMethod.POST, "/usuario/cadastrar").permitAll();
+			requisicoes.anyRequest().authenticated();
+		});
+		http.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+		return configuration.getAuthenticationManager();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
